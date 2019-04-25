794f489df05f9be967204209aeba9c30c27a689d
renfeisun <renfeisun@gmail.com>
2019-04-24 20:05:30 -0700

    add index with non-stopwords version

--- src/main/java/edu/arizona/cs/QueryProcessor.java
+++ src/main/java/edu/arizona/cs/QueryProcessor.java
@@ -4,8 +4,11 @@
 package edu.arizona.cs;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 import edu.stanford.nlp.simple.Sentence;
@@ -18,10 +21,14 @@ public class QueryProcessor {
 	private String questions;
 	private HashMap<String, LinkedList<QueryClass>> queryKey;
 	private int total = 0;
+	private ArrayList<String> stopWordsList;
+	private final String[] stopWords = {"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"};
+	
 	
 	public QueryProcessor(String file) {
 		questions = file;
 		queryKey = new HashMap<>();
+		stopWordsList = new ArrayList<>(Arrays.asList(stopWords));
 	}
 	
 	public HashMap<String, LinkedList<QueryClass>> getQueryKey(){
@@ -43,9 +50,9 @@ public class QueryProcessor {
 				String question = scanner.nextLine().replaceAll("[^ a-zA-Z\\d]", " ").trim();
 				String answer = scanner.nextLine();
 				stc = new Sentence(category);
-				category = String.join(" ", stc.lemmas());
+				category = removeStopWords(stc.lemmas());//String.join(" ", stc.lemmas());
 				stc = new Sentence(question);
-				question = String.join(" ", stc.lemmas());
+				question = removeStopWords(stc.lemmas());//String.join(" ", stc.lemmas());
 				pushIntoMap(category, question, answer);
 				scanner.nextLine();
 			}
@@ -63,6 +70,15 @@ public class QueryProcessor {
 			queryKey.put(answer, qc);	
 		}
 		queryKey.get(answer).add(new QueryClass(query, cat));
+	}
+	
+	private String removeStopWords(List<String> list){
+		String str = "";
+		for(String lemma : list){
+			if(!stopWordsList.contains(lemma))
+				str += lemma + " ";
+		}
+		return str.trim();
 	}
 	
 //	public String toString() {
